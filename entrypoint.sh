53008d18977b4e5b6edf569dbe8993de1aa88f12
Judson Bomfim <falecom@judsonbomfim.com>
2024-01-04 11:21:18 -0300

    Correções

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn core.wsgi:application --bind 0.0.0.0:80 --workers 4 --timeout 120 --log-level info
+gunicorn core.wsgi:application --bind 0.0.0.0:80 --timeout 120 --log-level info
