353a31c151f432841ec37a73be3773f6cca8fc09
Judson Bomfim <falecom@judsonbomfim.com>
2024-01-04 12:50:50 -0300

    Retirado ngnx

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
+gunicorn core.wsgi:application --bind 0.0.0.0:80 --workers 2 --threads 2 --timeout 120 --log-level=info
